839aac282b5d1d43c015ccc8fab4516312232ee4
cowglow <cowglow@gmail.com>
2019-01-02 22:41:26 +0100

    Update e2e testes and Travis-CI integration

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display partition label', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to secret-santa!');
+    expect(page.getTitleText()).toEqual('EMPLOYEES');
   });
 });
