bfb736b5cd97ffe7f58191a137fcfef722a8aa07
Philip Saa <cowglow@gmail.com>
2019-07-20 19:28:21 +0200

    JSCC19 Refactor - Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,11 +22,14 @@ export class AppComponent {
     SecretSantaList = [];
 
     RandomizeParticipants(participants) {
-        const results = [];
-        participants.forEach((participant, index) => {
-            results[Math.floor(Math.random() * (index + 1))] = participant;
-        });
-        return results;
+        for (let i = participants.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = participants[i];
+            participants[i] = participants[j];
+            participants[j] = temp;
+        }
+
+        return participants;
     }
 
     SecretSanta() {
@@ -34,56 +37,27 @@ export class AppComponent {
 
         const _participants = this.RandomizeParticipants(participants);
 
-        const _sort = _participants.map((participant, index, all) => {
-            return {
-                participant: participant,
-                santa: this.RandomizeParticipants(all.filter(santa => santa.lastname !== participant.lastname)[0])
-            };
-        });
+        for (let iv = 0; iv < _participants.length; iv++) {
+            if (this.ParticipantList[iv].lastname === participants[iv].lastname) {
+                participants[iv] = participants.pop();
+            }
+        }
 
-        /* Randomize Cloned Array */
-        // for (let i = list.length - 1; i > 0; i--) {
-        //   let j = Math.floor(Math.random() * (i + 1));
-        //   let temp = list[i];
-        //   list[i] = list[j];
-        //   list[j] = temp;
-        // }
+        // Validate Sort
+        for (let v = 0; v < _participants.length; v++) {
+            if (this.ParticipantList[v].lastname === _participants[v].lastname) {
+                this.SecretSanta();
+                return;
+            }
+        }
 
-        /* Assign Secret Santa */
-        // for (let ii = 0; ii < list.length; ii++) {
-        //   if (this.ParticipantList[ii].lastname !== list[ii].lastname) {
-        //     _list.push(list[ii]);
-        //   } else {
-        //     _list.push({});
-        //     _sort.push(list[ii]);
-        //   }
-        // }
+        // Validate length match
+        if (_participants.length !== this.ParticipantList.length) {
+            this.SecretSanta();
+            return;
+        }
 
-        /* Fix Empty Slots */
-        // for (let iii = 0; iii < _list.length; iii++) {
-        //   if (_list[iii].firstname === undefined) {
-        //     _list[iii] = _sort.pop();
-        //   }
-        // }
-
-        /* Repopulate Array */
-        // for (let iv = 0; iv < _sort.length; iv++) {
-        //   if (_sort[iv].lastname !== undefined) {
-        //     if (this.ParticipantList[iv].lastname === _list[iv].lastname) {
-        //       _list[iv] = _list.pop();
-        //     }
-        //   }
-        // }
-
-        /* Last Varification */
-        // for (let v = 0; v < _list.length; v++) {
-        //   if (this.ParticipantList[v].lastname === _list[v].lastname) {
-        //     this.SecretSanta();
-        //     return;
-        //   }
-        // }
-
-        this.SecretSantaList = _sort;
+        this.SecretSantaList = _participants;
     }
 
     AddParticipant() {
